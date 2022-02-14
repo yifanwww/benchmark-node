1a8d9e2af940d5df3dd5da0b448dc213faa16017
yifanwww <yifanw1101@gmail.com>
2022-02-14 22:29:16 +0800

    refactor: Delete unused code

--- src/Mathematics/__tests__/Mathematics.test.ts
+++ src/Mathematics/__tests__/Mathematics.test.ts
@@ -1,19 +1,15 @@
 import { Mathematics } from '../Mathematics';
 
-describe(`Test method \`${Mathematics.name}${Mathematics.sum.name}\``, () => {
-    it('returns average value', () => {
+describe(`Test class ${Mathematics}`, () => {
+    it(`tests static method ${Mathematics.sum.name}`, () => {
         expect(Mathematics.sum([1, 2, 3, 4, 4])).toBe(14);
     });
-});
 
-describe(`Test method \`${Mathematics.name}${Mathematics.mean.name}\``, () => {
-    it('returns average value', () => {
+    it(`tests static method ${Mathematics.mean.name}`, () => {
         expect(Mathematics.mean([1, 2, 3, 4, 4])).toBe(2.8);
     });
-});
 
-describe(`Test method \`${Mathematics.name}${Mathematics.variance.name}\``, () => {
-    it('returns variance value', () => {
+    it(`tests static method ${Mathematics.variance.name}`, () => {
         expect(Mathematics.variance([1, 2, 3, 4, 4], 2.8)).toBe(1.7);
     });
 });
