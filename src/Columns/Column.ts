edaa3da7adf68c8dcfd068c08c03feff90346705
yifanwww <yifanw1101@gmail.com>
2022-02-16 01:57:28 +0800

    fix: Fix unit types of statistic columns

--- src/Columns/Column.ts
+++ src/Columns/Column.ts
@@ -8,62 +8,52 @@ export class Column {
         'Mean',
         'Arithmetic mean of all measurements',
         (stats) => stats.mean,
-        UnitType.Dimensionless,
+        UnitType.Time,
     );
 
     public static readonly Error = new StatisticColumn(
         'Error',
         'Half of 99.9% confidence interval',
         (stats) => new ConfidenceInterval(stats.n, stats.mean, stats.standardError, ConfidenceLevel.L999).margin,
-        UnitType.Dimensionless,
+        UnitType.Time,
     );
 
     public static readonly StdDev = new StatisticColumn(
         'StdDev',
         'Standard deviation of all measurements',
         (stats) => stats.standardDeviation,
-        UnitType.Dimensionless,
+        UnitType.Time,
     );
 
     public static readonly StdErr = new StatisticColumn(
         'StdErr',
         'Standard error of all measurements',
         (stats) => stats.standardError,
-        UnitType.Dimensionless,
+        UnitType.Time,
     );
 
-    public static readonly Min = new StatisticColumn(
-        'Min',
-        'Minimum measurement',
-        (stats) => stats.min,
-        UnitType.Dimensionless,
-    );
+    public static readonly Min = new StatisticColumn('Min', 'Minimum measurement', (stats) => stats.min, UnitType.Time);
 
     public static readonly Q1 = new StatisticColumn(
         'Q1',
         'Quartile 1 (25th percentile)',
         (statss) => statss.q1,
-        UnitType.Dimensionless,
+        UnitType.Time,
     );
 
     public static readonly Median = new StatisticColumn(
         'Median',
         'Value separating the higher half of all measurements (50th percentile)',
         (statss) => statss.median,
-        UnitType.Dimensionless,
+        UnitType.Time,
     );
 
     public static readonly Q3 = new StatisticColumn(
         'Q3',
         'Quartile 3 (75th percentile)',
         (stats) => stats.q3,
-        UnitType.Dimensionless,
+        UnitType.Time,
     );
 
-    public static readonly Max = new StatisticColumn(
-        'Max',
-        'Maximum measurement',
-        (stats) => stats.max,
-        UnitType.Dimensionless,
-    );
+    public static readonly Max = new StatisticColumn('Max', 'Maximum measurement', (stats) => stats.max, UnitType.Time);
 }
