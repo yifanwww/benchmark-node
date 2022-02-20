24784a5f4dd7218bc0131925830d095b301e6407
yifanwww <yifanw1101@gmail.com>
2022-02-20 23:40:46 +0800

    fix: Enable chalk color forcely when doing unit tests

--- src/Tools/__tests__/TimeUnit.test.ts
+++ src/Tools/__tests__/TimeUnit.test.ts
@@ -1,28 +1,28 @@
 import { TimeUnit, TimeUnitHelper } from '../TimeUnit';
 
 describe(`Test class \`${TimeUnitHelper.name}\``, () => {
-    it(`calls static method ${TimeUnitHelper.getUnitStr.name}`, () => {
+    it('calls static method getUnitStr', () => {
         expect(TimeUnitHelper.getUnitStr(TimeUnit.NS)).toMatchSnapshot();
         expect(TimeUnitHelper.getUnitStr(TimeUnit.US)).toMatchSnapshot();
         expect(TimeUnitHelper.getUnitStr(TimeUnit.MS)).toMatchSnapshot();
         expect(TimeUnitHelper.getUnitStr(TimeUnit.S)).toMatchSnapshot();
     });
 
-    it(`calls static method ${TimeUnitHelper.getUnitDescription.name}`, () => {
+    it('calls static method getUnitDescription', () => {
         expect(TimeUnitHelper.getUnitDescription(TimeUnit.NS)).toMatchSnapshot();
         expect(TimeUnitHelper.getUnitDescription(TimeUnit.US)).toMatchSnapshot();
         expect(TimeUnitHelper.getUnitDescription(TimeUnit.MS)).toMatchSnapshot();
         expect(TimeUnitHelper.getUnitDescription(TimeUnit.S)).toMatchSnapshot();
     });
 
-    it(`calls static method ${TimeUnitHelper.getFullDescription.name}`, () => {
+    it('calls static method getFullDescription', () => {
         expect(TimeUnitHelper.getFullDescription(TimeUnit.NS, 6)).toMatchSnapshot();
         expect(TimeUnitHelper.getFullDescription(TimeUnit.US, 6)).toMatchSnapshot();
         expect(TimeUnitHelper.getFullDescription(TimeUnit.MS, 6)).toMatchSnapshot();
         expect(TimeUnitHelper.getFullDescription(TimeUnit.S, 6)).toMatchSnapshot();
     });
 
-    it(`calls static method ${TimeUnitHelper.chooseUnit.name}`, () => {
+    it('calls static method chooseUnit', () => {
         expect(TimeUnitHelper.chooseUnit(0)).toBe(TimeUnit.NS);
         expect(TimeUnitHelper.chooseUnit(1e3)).toBe(TimeUnit.NS);
         expect(TimeUnitHelper.chooseUnit(1e3 + 1)).toBe(TimeUnit.US);
